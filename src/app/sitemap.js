42e01646c3948ee96b29efbabdf1df2e5fc929f8
Irbanology <khanirban705@gmail.com>
2026-01-22 16:49:52 +0500

    Update robots.txt sitemap URL and refine Newsletter component heading

---
+++ src/app/sitemap.js
@@ -0,0 +1,20 @@
+export default function sitemap() {
+    return [
+        {
+            url: "https://wibeit.co",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://wibeit.co/policies/privacy_policy",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://wibeit.co/policies/child_safety",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://wibeit.co/policies/legal",
+            lastModified: new Date(),
+        },
+    ];
+}  
